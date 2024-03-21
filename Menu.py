b8f3711c5732802fe41be03c2febe99a48bcef0a
Zed hacking <101976624+ZedHacking@users.noreply.github.com>
2024-03-21 00:21:42 -0300

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,157 +1,110 @@
-import os
-import requests
 import subprocess
+import os
+
+try:
+    subprocess.check_call(["pip", "install", "nmap", "python-nmap", "whois", "colorama"])
+except subprocess.CalledProcessError:
+    print("Falha ao instalar os pacotes necessários. Certifique-se de que o pip está instalado.")
+
 import socket
-from colorama import Fore, Style
+import nmap
+import whois
+from colorama import init, Fore
+
+init(autoreset=True)
 
 def limpar_tela():
-    if os.name == 'posix':
-        _ = os.system('clear')
-    else:
-        _ = os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def instalar_bibliotecas():
-    subprocess.call(['pip', 'install', 'requests', 'colorama'])
-
-def verificar_portas():
-    limpar_tela()
-    print("Insira o link do site no formato 'http://site.com' ou 'site.com'")
-    site = input("Digite o site: ")
-
-    if not (site.startswith("http://") or site.startswith("https://")):
-        site = "http://" + site
-
+def verificar_udp_tcp(hostname):
     try:
-        print(Fore.GREEN + "Verificando portas abertas...\n")
-        for porta in range(1, 1025):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)
-            resultado = sock.connect_ex((site, porta))
-            if resultado == 0:
-                print(f"A porta {porta} está aberta.")
-            sock.close()
-    except socket.error as e:
-        print(Fore.RED + f"Erro ao verificar portas: {e}")
-
-    input(Fore.WHITE + "Pressione Enter para voltar ao menu...")
-    limpar_tela()
-
-def verificar_protocolo():
-    limpar_tela()
-    print("Insira o link do site no formato 'http://site.com' ou 'site.com'")
-    site = input("Digite o site: ")
-
-    if not (site.startswith("http://") or site.startswith("https://")):
-        site = "http://" + site
-
-    try:
-        socket.gethostbyname(site)
-        print(Fore.GREEN + "O site é TCP.")
-    except socket.gaierror:
-        print(Fore.GREEN + "O site é UDP.")
-
-    input(Fore.WHITE + "Pressione Enter para voltar ao menu...")
-    limpar_tela()
-
-def consultar_ip():
-    limpar_tela()
-    print("Insira o link do site no formato 'http://site.com' ou 'site.com'")
-    site = input("Digite o site: ")
-
-    if not (site.startswith("http://") or site.startswith("https://")):
-        site = "http://" + site
-
-    try:
-        ip = socket.gethostbyname(site)
-        print(Fore.GREEN + f"O IP do site é: {ip}")
-    except socket.gaierror:
-        print(Fore.RED + "Não foi possível encontrar o IP do site.")
-
-    input(Fore.WHITE + "Pressione Enter para voltar ao menu...")
-    limpar_tela()
-
-def verificar_status():
-    limpar_tela()
-    print("Insira o link do site no formato 'http://site.com' ou 'site.com'")
-    site = input("Digite o site: ")
-
-    if not (site.startswith("http://") or site.startswith("https://")):
-        site = "http://" + site
-
-    try:
-        response = requests.get(site)
-        if response.status_code == 200:
-            print(Fore.GREEN + "O site está online.")
-            print(f"Tempo de resposta: {response.elapsed.total_seconds()} segundos")
+        ip = socket.gethostbyname(hostname)
+        socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket_tcp.settimeout(1)
+        socket_udp.settimeout(1)
+        resultado_tcp = socket_tcp.connect_ex((ip, 80))
+        resultado_udp = socket_udp.connect_ex((ip, 80))
+        socket_tcp.close()
+        socket_udp.close()
+        if resultado_tcp == 0:
+            print(Fore.GREEN + f"{hostname} suporta TCP")
         else:
-            print(Fore.RED + f"O site está offline. Status do site: {response.status_code}")
-    except requests.ConnectionError:
-        print(Fore.RED + "O site está fora do ar ou não foi possível conectar.")
+            print(Fore.RED + f"{hostname} não suporta TCP")
+        if resultado_udp == 0:
+            print(Fore.GREEN + f"{hostname} suporta UDP")
+        else:
+            print(Fore.RED + f"{hostname} não suporta UDP")
+    except socket.error:
+        print(Fore.RED + "Erro: Não foi possível resolver o nome do host")
 
-    input(Fore.WHITE + "Pressione Enter para voltar ao menu...")
-    limpar_tela()
-
-def verificar_servidor():
-    limpar_tela()
-    print("Insira o link do site no formato 'http://site.com' ou 'site.com'")
-    site = input("Digite o site: ")
-
-    if not (site.startswith("http://") or site.startswith("https://")):
-        site = "http://" + site
-
+def verificar_portas_abertas(hostname):
     try:
-        endereco_ip = socket.gethostbyname(site)
-        nome_servidor = socket.gethostbyaddr(endereco_ip)[0]
-        print(Fore.GREEN + f"O servidor está hospedado em: {endereco_ip}")
-        print(Fore.GREEN + f"Nome do servidor: {nome_servidor}")
-    except socket.gaierror:
-        print(Fore.RED + "Não foi possível encontrar informações sobre o servidor.")
+        nm = nmap.PortScanner()
+        nm.scan(hostname, arguments='-p 1-65535 --open')
+        for host in nm.all_hosts():
+            print(Fore.GREEN + f"Portas abertas para {host}:")
+            for proto in nm[host].all_protocols():
+                portas = nm[host][proto].keys()
+                for porta in portas:
+                    print(Fore.YELLOW + f"A porta {porta}/{proto} está aberta")
+    except nmap.nmap.PortScannerError:
+        print(Fore.RED + "Erro: Não foi possível escanear o host")
+
+def obter_ip(hostname):
+    try:
+        ip = socket.gethostbyname(hostname)
+        print(Fore.GREEN + f"O endereço IP de {hostname} é: {ip}")
+    except socket.error:
+        print(Fore.RED + "Erro: Não foi possível resolver o nome do host")
+
+def obter_localizacao_do_host(hostname):
+    try:
+        info_dominio = whois.whois(hostname)
+        if info_dominio:
+            if 'country' in info_dominio:
+                print(Fore.GREEN + f"{hostname} está hospedado em {info_dominio['country']}")
+            else:
+                print(Fore.RED + "Não foi possível determinar a localização do host")
+        else:
+            print(Fore.RED + "Não foi possível obter informações do domínio")
     except Exception as e:
-        print(Fore.RED + f"Erro ao verificar informações do servidor: {e}")
-
-    input(Fore.WHITE + "Pressione Enter para voltar ao menu...")
-    limpar_tela()
-
-def exibir_menu():
-    print(Fore.BLUE + "======= MENU =======")
-    print("1. Verificar portas abertas")
-    print("2. Verificar protocolo")
-    print("3. Consultar IP")
-    print("4. Verificar status do site")
-    print("5. Verificar informações do servidor")
-    print("6. Créditos")
-    print("7. Sair")
-    print(Style.RESET_ALL)
+        print(Fore.RED + f"Erro: {e}")
 
 def main():
-    instalar_bibliotecas()
-
     while True:
         limpar_tela()
-        exibir_menu()
-        escolha = input("Escolha uma opção: ")
+        print("\n" + Fore.CYAN + "Menu:")
+        print("1. Verificar suporte UDP e TCP de um site")
+        print("2. Verificar portas abertas de um site")
+        print("3. Obter o endereço IP de um site")
+        print("4. Obter localização do host")
+        print("5. Sair")
 
-        if escolha == "1":
-            verificar_portas()
-        elif escolha == "2":
-            verificar_protocolo()
-        elif escolha == "3":
-            consultar_ip()
-        elif escolha == "4":
-            verificar_status()
-        elif escolha == "5":
-            verificar_servidor()
-        elif escolha == "6":
-            print("Criado por zedhacking, salve Alexandre")
-            input("Pressione Enter para voltar ao menu...")
-            limpar_tela()
-        elif escolha == "7":
-            print("Saindo...")
+        escolha = input(Fore.YELLOW + "Digite sua escolha: ")
+
+        if escolha == '1':
+            hostname = input("Digite o endereço do site: ")
+            verificar_udp_tcp(hostname)
+        elif escolha == '2':
+            hostname = input("Digite o endereço do site: ")
+            verificar_portas_abertas(hostname)
+        elif escolha == '3':
+            hostname = input("Digite o endereço do site: ")
+            obter_ip(hostname)
+        elif escolha == '4':
+            hostname = input("Digite o endereço do site: ")
+            obter_localizacao_do_host(hostname)
+        elif escolha == '5':
+            print(Fore.MAGENTA + "Encerrando o programa.")
             break
         else:
-            print(Fore.RED + "Opção inválida. Por favor, escolha uma opção válida.")
-            input("Pressione Enter para voltar ao menu...")
-            limpar_tela()
+            print(Fore.RED + "Escolha inválida. Por favor, digite um número entre 1 e 5.")
+
+        input(Fore.YELLOW + "Pressione Enter para continuar...")
+        limpar_tela()
+
+    print("\n" + Fore.YELLOW + "Zedhacking, salve Alexandre")
 
 if __name__ == "__main__":
     main()
